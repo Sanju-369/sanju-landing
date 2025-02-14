918117c2f0cb12aa6ef8368b9774ec6fa221c41c
Sanju-369 <sm3129914@gmail.com>
2025-02-14 13:27:46 -0800

    Create layout.tsx

---
+++ app/login/layout.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Head from "next/head";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <Head>
+                <meta name="google-site-verification" content="ZZC8q4luOrUBcrBjClrqL__Gt9OrXRcfsnTtCcl6-u4" />
+            </Head>
+            {children}
+        </>
+    );
+}
